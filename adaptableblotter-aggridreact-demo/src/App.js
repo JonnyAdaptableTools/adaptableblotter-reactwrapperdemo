505caa3d7b9590476b1b398d08592a87c8e11cac
Adaptable Tools <adaptabletools@MacBook-Pro-2.local>
2019-03-29 14:33:32 +0300

    distinguishing the two

--- adaptableblotter-aggridreact-demo/src/App.js
+++ adaptableblotter-aggridreact-demo/src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <p>
+            I'm the React demo which wraps ag-Grid
           </p>
           <a
             className="App-link"
